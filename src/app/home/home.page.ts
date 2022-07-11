f8ee700e8dc745207750a4153a4f8becf12727e9
Andrew Bargan <andrew.bargan@gmail.com>
2022-07-11 11:37:14 +0300

    bug

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,73 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(
+    private platform: Platform
+  ) {
 
+    this.init();
+  }
+
+  async init() {
+
+    await this.platform.ready();
+
+    this.readSim();
+  }
+
+  async readSim() {
+
+    return this.platform.is('android') ?
+      this.requestAndroidSimPermissions() :
+      this.getSimInfo();
+  }
+
+  requestAndroidSimPermissions() {
+
+    window['plugins'].sim.hasReadPermission(
+      success => {
+
+        console.log('hasReadPermission', success);
+
+        this.getSimInfo()
+      },
+      error => {
+
+        console.log('hasReadPermission', error);
+
+        window['plugins'].sim.requestReadPermission(
+          isGranted => {
+
+            console.log('sim.requestReadPermission(): ', isGranted);
+
+            return this.getSimInfo();
+          },
+          error => {
+
+            console.log('sim.requestReadPermission() error: ', error);
+
+            return this.getSimInfo();
+          }
+        );
+      }
+    );
+  }
+
+  getSimInfo() {
+
+    window['plugins'].sim.getSimInfo(
+      info => {
+
+        console.log('sim.getSimInfo(): ', info);
+
+        return info;
+      },
+      error => {
+
+        console.log('sim.getSimInfo() error: ', error);
+
+        return false;
+      }
+    );
+  }
 }
